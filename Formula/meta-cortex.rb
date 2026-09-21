b62da5c46144a51e9bb2b2b3dfdfb2b34640cfae
Meta-Cortex releases <41898282+github-actions[bot]@users.noreply.github.com>
2026-09-21 00:52:04 +0000

    Update Meta-Cortex to latest release

--- Formula/meta-cortex.rb
+++ Formula/meta-cortex.rb
@@ -1,25 +1,25 @@
 class MetaCortex < Formula
   desc "Install the Meta-Cortex development framework in a project"
   homepage "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex"
-  version "0.1.1"
+  version "0.1.2"
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.1/meta-cortex-aarch64-apple-darwin.tar.xz"
-      sha256 "10c4241678c2b2f5357bf41ee6cb71bc8fad6563f87f5270a7cc4ebfba5e361b"
+      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.2/meta-cortex-aarch64-apple-darwin.tar.xz"
+      sha256 "72547a030ead035bebc8bd120c3f1fd11936765ba980992441737281ed74c4b5"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.1/meta-cortex-x86_64-apple-darwin.tar.xz"
-      sha256 "9ee2ad2f2c46a868a70074c4f6ab855469cf7ede7e2cb2c944d06fbe2d19a5c7"
+      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.2/meta-cortex-x86_64-apple-darwin.tar.xz"
+      sha256 "a6a2bc0f471b7a17eb364a2c1eff6245cf2d0e243b24a80e2441b29a0b7bff5d"
     end
   end
   if OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.1/meta-cortex-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "2974c3617940e47463f1963d13c7eea7da67d1145b2529f4f38313f60eaf0286"
+      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.2/meta-cortex-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "69013d1311d90f37988b2ef2726c05c36ced8cbcb35e11e047df3bc2c0407d3b"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.1/meta-cortex-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "595be2ee68f4c638d0e6160ea0e4080abd6e98f9cd58b0e702d6fe8478f638cf"
+      url "https://github.com/ai-ai-ai-ai-ai-ai-ai/meta-cortex/releases/download/v0.1.2/meta-cortex-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "4094e5fd4f4d8c48e2b1e8b02c83ebd9bdef5722cb0cfad2c8dd62252cfc3253"
     end
   end
   license "Apache-2.0"
